2e7a643e70ec17cc291675ab614c10ac154653bd
Ícaro Maia Santos de Castro <icaromscastro@gmail.com>
2016-11-17 02:51:49 -0200

    atualizações do simulador
    -->implementado algoritmo de CRC
    -->implementado algoritmo de padding
    -->implementada funcao main(aplicacao)
    -->implementado algoritmo de simulacao de ruido

--- simulador_quadro_ethernet.py
+++ simulador_quadro_ethernet.py
@@ -1,4 +1,4 @@
-import binascii as bin
+import binascii as crc
 import threading
 import time
 import random
@@ -18,24 +18,55 @@ class QuadroEthernet(object):
     
     '''
     def __init__(self):
-        self.preambulo=None
+        self.preambulo='10101010'
         self.macDestino=None
         self.macOrigem=None
-        self.tipo=None
+        self.tamQuadro=0
         self.dados=None
         self.FCS=None
-    
+        self.pad=0
+        self.tamMin=64
+        self.tamMax=1500
 
+
+    def obterTamQuadro(self):
+        #gera tamanhos para o cabeçalho do quadro
+        pream=7
+        tOri=random.randint(2,6)
+        tDes=random.randint(2,6)
+        SD=1
+        length=2
+        tDados=len(self.dados)
+        crc=4
+        self.tamQuadro=pream + tOri + tDes + SD + length + tDados + crc
+        #algoritmo padding
+        if(self.tamQuadro<self.tamMin):
+            self.padding()
+
+        
+    def crc32(self):
+        return crc.crc32(self.dados)
+        
+    
+    def padding(self):
+        self.pad=self.tamMin-self.tamQuadro
+
+            
+    def exibir(self):
+        print 'Quadro Ethernet: \n',' preambulo---> ',self.preambulo,'\n mac destino--->',self.macDestino,'\n mac origem--->',self.macOrigem,'\n dados---> ',self.dados
 
 
 
 
 class Meio(object):
     '''classe responsavel por compartilhar um quadro entre cliente origem e destino'''
-    def __main__():
+    def __init__(self):
         self.quadro=None
+
         
-        
+    def ocupado(self):
+        return self.quadro!=None
+
     
     def addQuadro(self,q):
         '''
@@ -60,7 +91,6 @@ class Meio(object):
             return False
             
             
-    
     def retiraQuadro(self):
         '''
         Descrição:
@@ -81,10 +111,44 @@ class Meio(object):
             self.quadro=None
             return quadro
 
+
+    def transmitirQuadro(self):
+        chance=random.randint(0, 1)
+        if chance==1:
+            print 'houve ruido durante a transmissão do quadro'
+            self.__geraRuido()
+
+
+    def __geraRuido(self):
+        temp=self.quadro.dados
+        pos=random.randint(0,(len(temp)-1))
+        ruido=temp.replace(temp[pos],'?')
+        self.quadro.dados=ruido
+        #print ruido
+        
+        
+            
+
 class ClienteDestino(object):
 
     def __init__(self,mac):
         self.mac=mac
     
-        
+def aplicacao():
+    print '-----------SIMULADOR QUADRO ETHERNET-----------------'
+    
+    #instancia quadro e canal 
+    q=QuadroEthernet()
+    canal=Meio()
+    origem='00:1D:7D:B2:34:F9'
+    destino='00:1D:7D:W4:86:G5'
+    #recupera dados do usuario
+    q.macOrigem=origem
+    q.macDestino=destino
+    q.dados=raw_input('informe a mensagem que deseja transmitir:')
+    q.exibir()
+    
+
+aplicacao()
+    
         
